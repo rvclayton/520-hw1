708380aefdaa96ee49dabc8d227f23f1d69ed0d5
r. clayton <rvclayton@acm.org>
2015-11-02 08:01:02 -0500

    define the log animator, use it on bfs

--- hw1graph.ts
+++ hw1graph.ts
@@ -2,8 +2,13 @@
 /// <reference path = "utils.ts"/>
 
 
+var backgroundColor = 'black';
+var foregroundColor = 'white';
+  
+
 function
-drawGraph(g: Graph, paper, ht, wd, ul: Coordinate): void {
+drawGraph(
+  g: Graph, paper, ht, wd, ul: Coordinate, logAnimator: AnimateLog): void {
 
 
   var drawEdge = function (e): void {
@@ -11,9 +16,7 @@ drawGraph(g: Graph, paper, ht, wd, ul: Coordinate): void {
     var fromC = transCoord(e.fromNode.center);
     var toC = transCoord(e.toNode.center);
 
-    paper.path(
-      'M' + fromC.x() + ' ' + fromC.y() + 'L' + toC.x() + ' ' + toC.y())
-      .attr({ stroke: foregroundColor });
+    drawLine(paper, fromC.x(), fromC.y(), toC.x(), toC.y());
 
     var lx = (fromC.x() + toC.x())/2;
     var ly = (fromC.y() + toC.y())/2;
@@ -55,11 +58,26 @@ drawGraph(g: Graph, paper, ht, wd, ul: Coordinate): void {
   var nodeRadius = Math.min(ht, wd)*0.075;
   var nodeFontSize = nodeRadius*0.7;
   
-  var backgroundColor = 'black';
-  var foregroundColor = 'white';
-  
+  var clickPad = paper.rect(0, 0, ht, wd)
+    .attr('fill', backgroundColor)
+    .data('clickCount', 0)
+    .data('logAnimator', logAnimator)
+    .click(function (e) {
+      var cc = this.data('clickCount')
+      console.log('click ' + ++cc);
+      this.data('clickCount', cc);
+      this.data('logAnimator').step();
+      });
+      
   g.edgeSetIterate(drawEdge);
   g.nodeSetIterate(drawNode);
+  }
+
+
+function
+drawLine(paper, x1, y1, x2, y2) {
+  paper.path('M' + x1 + ' ' + y1 + 'L' + x2 + ' ' + y2)
+    .attr({ stroke: foregroundColor });
   }
 
 
@@ -96,20 +114,28 @@ makeGraph(): Graph {
 
 
 function
-BFSGraphSearch(g: Graph): void {
+BFGraphSearch(g: Graph): string [] {
 
-  var queue = [ g.startState() ];
-
+  var q = new Queue();
+  q.enq(g.startState());
+  var visited = { };
+  
   do {
-    var node = queue.shift();
+    var node = q.deq();
     if (g.isGoalState(node))
-      return;
+      return q.log();
 
     var neighbors = g.neighbors(node);
-    for (var i = neighbors.length - 1; i > -1; --i)
-      queue.push(neighbors[i]);
+    neighbors.sort();
+    for (var i = 0; i < neighbors.length; ++i) {
+      var n = neighbors[i];
+      if (!visited[n]) {
+        q.enq(n);
+	visited[n] = true;
+	}
+      }
     }
-  while (queue.length > 0);
+  while (!q.empty());
 
   alert("graph bfs didn't find a goal state")
   }
@@ -221,4 +247,182 @@ ASGraphSearch(g: Graph): void {
   while (queue.length > 0);
 
   alert("graph A* search didn't find a goal state")
+  }
+
+
+class
+AnimateLog {
+
+  constructor(paper, ht, wd, ul: Coordinate, g: Graph, log) {
+    this.paper = paper;
+    this.ht = ht;
+    this.wd = wd;
+    this.ul = new Coordinate(ul.x() + 10, ul.y());
+    this.g = g;
+    this.log = log;
+
+    this.nextStep = 0;
+    this.q = [];
+    this.qRep = paper.text(ul.x(), ul.y(), "");
+    this.fontSize = Math.round(ht*0.6);
+    }
+
+
+  private
+  drawQ() {
+
+    var qStr = ""
+    var sep = ""
+
+    for (var i = 0; i < this.q.length; ++i) {
+      qStr = qStr + sep + this.q[i]
+      sep = "  "
+      }
+
+    console.log('draw this: "' + qStr + '" at ' + this.ul.toString() + '.');
+    
+    return this.paper.text(this.ul.x(), this.ul.y(), qStr).attr({
+        stroke: foregroundColor
+      , fill: foregroundColor
+      , 'font-size': this.fontSize
+      , 'text-anchor': "start"
+      });
+    }
+
+
+  step() {
+    if (this.nextStep == 0)
+      this.q = [ ];
+
+    if (this.log[this.nextStep][0] == '-')
+      this.q.shift();
+    else if (this.log[this.nextStep][0] == '+')
+      this.q.push(this.log[this.nextStep][1]);
+    else
+      throw new Error('unrecognized operator in log');
+      
+    this.qRep.remove();
+    this.qRep = this.drawQ();
+
+    this.nextStep = (this.nextStep + 1) % this.log.length;
+    }
+
+
+  private log: any [];
+  private paper;
+  private ht: number;
+  private wd: number;
+  private ul: Coordinate;
+  private g: Graph;
+
+  private nextStep: number;
+  private q: string [ ];
+  private qRep;
+  private fontSize;
+  }
+
+
+class
+Queue {
+
+
+  deq() {
+    var e = this.q.shift()
+    this.script.push(['-']);
+    return e;
+    }
+
+
+  empty(): boolean {
+    return this.q.length == 0;
+    }
+
+
+  enq(n) {
+    this.q.push(n);
+    this.script.push(['+', n]);
+    }
+
+
+  log(): string [] {
+    return this.script;
+    }
+
+
+  private q = [ ];
+  private script = [];
+  }
+
+
+class
+QueuePicture {
+
+
+  constructor(paper, ht, wd, ul: Coordinate) {
+    drawLine(paper, ul.x(), ul.y(), ul.x() + wd, ul.y());  
+    drawLine(paper, ul.x(), ul.y(), ul.x(), ul.y() + ht);  
+    drawLine(paper, ul.x(), ul.y() + ht, ul.x() + wd, ul.y() + ht);  
+
+    this.fontSize = ht*0.8;
+    this.ul = ul;
+    this.ht = ht;
+    this.wd = wd;
+    this.paper = paper;
+    this.script = [ ];
+    }
+
+
+  deq() {
+    var e = this.q.shift()
+    this.script.push('-');
+    return e;
+    }
+
+
+  private
+  drawQ() {
+
+    for (var e in this.qElements)
+      this.qElements[e].remove();
+      
+    var x = this.ul.x();
+    var y = this.ul.y() + this.ht;
+    var trail = ""
+    
+    for (var i = 0; i < this.q.length; ++i) {
+      var e = this.q[i];
+      var p = this.paper.text(x, y, e).attr({
+          'font-size': this.fontSize
+        , stroke: foregroundColor
+        , fill: foregroundColor
+        });
+      var bb = p.getBBox();
+
+      trail = trail + " " + e;
+      x = x + bb.width + 5;
+      this.qElements[e] = p;
+      }
+
+    console.log('Q:' + trail);
+    }
+
+  empty(): boolean {
+    return this.q.length == 0;
+    }
+
+
+  enq(n) {
+    this.q.push(n);
+    this.script['+ ' + n];
+    }
+
+
+  private q = [ ];
+  private qElements = { };
+  private fontSize;
+  private ul;
+  private ht;
+  private wd;
+  private paper;
+  private script;
   }
