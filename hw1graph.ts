9d2bbb5a95bee95db7e96e514399d8fa50f2a838
r. clayton <rvclayton@acm.org>
2015-11-12 07:08:06 -0500

    split out log animator; define drawSlide()

--- hw1graph.ts
+++ hw1graph.ts
@@ -1,10 +1,8 @@
 /// <reference path = "Graph.ts"/>
 /// <reference path = "utils.ts"/>
+/// <reference path = "LogAnimator.ts"/>
+/// <reference path = "raphael.d.ts"/>
 
-
-var backgroundColor = 'black';
-var foregroundColor = 'white';
-  
 
 function
 ASGraphSearch(g: Graph): any [] {
@@ -155,7 +153,7 @@ drawGraph(
   , ht
   , wd
   , ul: Coordinate
-  , logAnimator: AnimateLog
+  , logAnimator: LogAnimator.T
   , options
   ): void {
 
@@ -228,9 +226,16 @@ drawGraph(
 
 
 function
-drawLine(paper, x1, y1, x2, y2) {
-  paper.path('M' + x1 + ' ' + y1 + 'L' + x2 + ' ' + y2)
-    .attr({ stroke: foregroundColor });
+drawSlide(divID, ht, wd, search, options) {
+
+  var g = makeGraph();
+  var qht = 50;
+  var paper = Raphael(document.getElementById(divID), wd, ht);
+  var log = search(g);
+  var logAnimator = new LogAnimator.T(
+    paper, qht, wd, new Coordinate(0, 0), g, log);
+  drawGraph(
+    g, paper, ht - qht, wd, new Coordinate(0, qht), logAnimator, options);
   }
 
 
@@ -524,7 +529,7 @@ SequenceItem {
     if (typeof arg == "string")
       this._name = arg;
     else if (typeof arg == "object") {
-      if (arg.name == "undefined")
+      if (arg.name == undefined)
         throw new Error("constructing a sequence item with no name");
       this._name = arg.name;
       this._pRep = arg.pRep;
@@ -553,158 +558,4 @@ SequenceItem {
   private _name;
   private _priority;
   private _pathCost;
-  }
-
-
-class
-AnimateLog {
-
-  // Draw a log animation within a rectangle.
-  
-  constructor(
-      paper		// where to draw the rectangle.
-    , ht		// the rectangle's height
-    , wd		// the rectangle's width.
-    , ul: Coordinate	// The rectangle's upper-left corner.
-    , g: Graph		// The graph involved.
-    , log		// The log to animate.
-    ) {
-
-    this.nextStep = 0;
-    
-    var fontSize = Math.round(ht*0.8);
-    var msgFontSize = Math.round(fontSize*0.5);
-
-    this.history = this.fillHistory(log);
-
-    var nameWd = this.findNameWd(paper, this.history, fontSize);
-    var gap = 5;
-    
-    this.poppedRep = paper.text(
-      ul.x() + gap + nameWd/2, ul.y() + ht*0.5, "").attr({
-        stroke: foregroundColor
-      , fill: foregroundColor
-      , 'font-size': fontSize
-      , 'text-anchor': "middle"
-      });
-    drawLine(paper,
-      ul.x() + 2*gap + nameWd, ul.y() + ht*0.1,
-      ul.x() + 2*gap + nameWd, ul.y() + ht*0.9)
-    this.qRep = paper.text(ul.x() + nameWd + 3*gap, ul.y() + ht*0.5, "").attr({
-        stroke: foregroundColor
-      , fill: foregroundColor
-      , 'font-size': fontSize
-      , 'text-anchor': "start"
-      });
-    this.msgRep = paper.text(ul.x() + wd - gap, ul.y() + ht*0.5, "")
-      .attr({
-	stroke: foregroundColor
-      , fill: foregroundColor
-      , 'font-size': msgFontSize
-      , 'text-anchor': "end"
-      })
-    }
-
-
-  private
-  drawQ(step) {
-
-    var qStr = ""
-    var sep = ""
-
-    for (var i = 0; i < step.q.length; ++i) {
-      qStr = qStr + sep + step.q[i]
-      sep = "  "
-      }
-
-    this.qRep.attr({ text: qStr });
-    this.poppedRep.attr({ text: step.lastPopped });
-    this.msgRep.attr({ text: step.msg });
-
-    console.log('drew "' + this.qRep.attr('text') + '".');
-    }
-
-
-  private
-  fillHistory(log): any [] {
-
-    var q = [];
-    var lastPopped = "";
-    var msg = "";
-    var history = [];
-    
-    for (var i = 0; i < log.length; ++i) {
-      var step = log[i];
-
-      if (step[0] == 'clear') {
-	q = [];
-	lastPopped = "";
-	msg = "";
-        }
-
-      else if (step[0] == 'deq')
-	lastPopped = q.shift();
-
-      else if (step[0] == 'enq')
-	q.push(step[1]);
-
-      else if (step[0] == 'msg')
-	msg = step[1];
-
-      else if (step[0] == 'pop')
-	lastPopped = q.shift();
-
-      else if (step[0] == 'push')
-	q.unshift(step[1]);
-
-      else if (step[0] == 'swap') {
-	var t = q[step[1]];
-	q[step[1]] = q[step[2]];
-	q[step[2]] = t;
-	}
-
-      else
-	throw new Error('unrecognized operator in log: ' + step[0]);
-
-      history.push({
-	  q: q.slice()
-	, lastPopped: lastPopped
-        , msg: msg
-        });
-      }
-
-    return history;
-    }
-
-
-  private
-  findNameWd(paper, log, fs: number) {
-
-    var nwd = -1;
-    var scratch = paper.text(-100, -100, 100, 100, "").attr('font-size', fs);
-
-    setIterate(log, function (n) {
-      scratch.attr('text', n.lastPopped);
-      var bb = scratch.getBBox();
-      nwd = Math.max(bb.width, nwd);
-      });
-    
-    scratch.remove();
-    
-    return nwd;
-    }
-
-
-  step() {
-    this.drawQ(this.history[this.nextStep]);
-    this.nextStep = (this.nextStep + 1) % this.history.length;
-    }
-
-
-  private nextStep: number; 
-  private history: any [];
-
-  private qRep;
-  private poppedRep;
-  private msgRep;
   }
