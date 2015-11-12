ed94effee68ea6b5b288f515b62182e0efb7c034
r. clayton <rvclayton@acm.org>
2015-11-12 07:10:40 -0500

    add drawLine() and the foreground and background colors.

--- utils.ts
+++ utils.ts
@@ -1,6 +1,18 @@
+var
+  backgroundColor = 'black',
+  foregroundColor = 'white';
+  
+
 function
 assert(test: boolean, msg: string): void {
   if (!test) throw new Error(msg);
+  }
+
+
+function
+drawLine(paper, x1, y1, x2, y2) {
+  return paper.path('M' + x1 + ' ' + y1 + 'L' + x2 + ' ' + y2)
+    .attr({ stroke: foregroundColor });
   }
 
 
